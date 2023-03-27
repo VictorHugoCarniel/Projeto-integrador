50791c315018cfb1e52b1eeff57aa2ae05ae7673
juyyaa <juliaamarxal@gmail.com>
2023-03-27 14:01:31 -0300

    cor botões carrinho

--- public/js/carrinho.js
+++ public/js/carrinho.js
@@ -65,10 +65,6 @@ if (typeof window === "object") {
   // code is running in a non-browser environment
 }
 
-function atualizaContador(){
-
-}
-
 function criarPedido(nome, preco) {
   const precoFormatado = retornaPreco(preco);
   const id = Math.random();
@@ -246,28 +242,7 @@ function gravaPedidos() {
 
 function enviaPedido() {
   let tagPedidos = document.querySelector("#pedidos");
-  tagPedidos.innerHTML += `
-      <div class="modal" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>Modal body text goes here.</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Save changes</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
-  console.log = "joasn"
+  console.log('sdfsdfsdfsd')
 }
 
 // function GetCard(cardBody){
